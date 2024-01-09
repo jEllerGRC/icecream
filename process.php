ad2c6248361cb73139d60a5bce7dbded8a656a76
Jared Eller <eller.jared@student.greenriver.edu>
2024-01-09 13:36:42 -0500

    Added price calculations

--- process.php
+++ process.php
@@ -11,6 +11,16 @@
         <h1>Thank you for your order!</h1>
 
         <?php
+
+        //Turn on error reporting
+        ini_set("display_errors", 1);
+        error_reporting(E_ALL);
+
+        //define constants
+        define("PRICE_PER_SCOOP", 2.50);
+        define("SALES_TAX_RATE", 0.11);
+
+            //echo raw variables from post array
             echo "<pre>";
             echo var_dump($_POST);
             echo "</pre>";
@@ -25,6 +35,16 @@
             echo "<p> $scoops scoops </p>";
             echo "<p>Flavors: $flavorString </p>";
             echo "<p>$cone cone </p>";
+
+            //calculate total due
+            $subtotal = PRICE_PER_SCOOP * $scoops;
+            $tax = $subtotal * SALES_TAX_RATE;
+            $total = $subtotal + $tax;
+
+            //Print totals
+            echo "<p>Subtotal: {$subtotal}</p>";
+            echo "<p>Tax: {$tax}</p>";
+            echo "<p>Total: {$total}</p>";
         ?>
 
     </body>
